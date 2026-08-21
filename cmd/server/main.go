a5ee993484f1ba4bf89360a35ce51433209e4dff
Erfan Ghorbani <erfan2ghorbani@gmail.com>
2026-08-21 15:34:34 +0330

    feat: setup routes and wire dependencies in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,27 @@ package main
 import (
 	"net/http"
 
+	"github.com/erfangho/url-shortener/internal/config"
+	"github.com/erfangho/url-shortener/internal/handler"
+	"github.com/erfangho/url-shortener/internal/repository"
+	"github.com/erfangho/url-shortener/internal/routes"
+	"github.com/erfangho/url-shortener/internal/service"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	db, err := config.InitDB()
+
+	if err != nil {
+		panic(err)
+	}
+
+	urlRepo := repository.NewURLRepository(db)
+	urlService := service.NewURLService(urlRepo)
+	urlHandler := handler.NewURLHandler(urlService)
+
+	routes.RegisterRoutes(r, urlHandler)
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -15,5 +31,8 @@ func main() {
 		})
 	})
 
-	r.Run()
+	err = r.Run()
+	if err != nil {
+		return
+	}
 }
---
+++ internal/routes/routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/erfangho/url-shortener/internal/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(router *gin.Engine, urlHandler *handler.URLHandler) {
+	urlGroup := router.Group("/urls")
+	{
+		urlGroup.POST("", urlHandler.CreateURL)
+		urlGroup.GET("/:shortCode", urlHandler.GetURL)
+	}
+
+	router.GET("/:shortCode", urlHandler.Redirect)
+}
