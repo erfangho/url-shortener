39864ddc9fa9b98c92f0c1dd8f964bf61194661a
Erfan Ghorbani <erfan2ghorbani@gmail.com>
2026-08-29 22:18:10 +0330

    feat: add gRPC client with RecordClick method

---
+++ internal/grpc/client.go
@@ -0,0 +1,33 @@
+package grpc
+
+import (
+	"context"
+
+	pb "github.com/erfangho/url-shortener/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type AnalyticsClient struct {
+	client pb.AnalyticsServiceClient
+	conn   *grpc.ClientConn
+}
+
+func NewAnalyticsClient(addr string) (*AnalyticsClient, error) {
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &AnalyticsClient{
+		client: pb.NewAnalyticsServiceClient(conn),
+		conn:   conn,
+	}, nil
+}
+
+func (c *AnalyticsClient) RecordClick(ctx context.Context, event *pb.ClickEvent) error {
+	_, err := c.client.RecordClick(ctx, event)
+
+	return err
+}
